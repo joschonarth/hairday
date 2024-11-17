6fa1cc4181c469b6745f4536059adb1b4a365013
joschonarth <joschonarth@gmail.com>
2024-11-16 22:05:03 -0300

    feat: update schedule list

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs"
 
 import { scheduleNew } from "../../services/schedule-new"
+import { schedulesDay } from "../schedules/load.js"
 
 const  form = document.querySelector("form")
 const clientName = document.getElementById("client")
@@ -38,6 +39,10 @@ form.onsubmit = async (event) => {
             name,
             when
         })
+
+        await schedulesDay()
+        
+        clientName.value = ""
 
     } catch (error) {
         alert("Não foi possível realizar o agendamento.")
